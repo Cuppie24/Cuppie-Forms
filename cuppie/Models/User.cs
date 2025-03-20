1d5d1f94c2e0a618be1b983b8cc84716658133d1
Ishnazarov <ishnazarovsaidnazar24@gmail.com>
2025-03-20 20:28:02 +0500

    Migration test

--- cuppie/Models/User.cs
+++ cuppie/Models/User.cs
@@ -4,9 +4,5 @@
     {
         public int Id { get; set; }
         public string? Username { get; set; }
-        public string? Login { get; set; }
-        public string? Email { get; set; }
-        public string? PassHash { get; set; }
-        public string? PassSalt { get; set; }
     }
 }
